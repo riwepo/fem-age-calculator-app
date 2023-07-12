f98634c64ace38afeb377a4c5e3159e4c6be26a0
Richard Potter <richardpotter.au@gmail.com>
2023-07-13 08:17:19 +1000

    implement basic skeleton of components

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Form from "./Form";
+import Display from "./Display";
+
+function Card() {
+  return (
+    <>
+      <Form />
+      <Display />
+    </>
+  );
+}
+
+export default Card;
