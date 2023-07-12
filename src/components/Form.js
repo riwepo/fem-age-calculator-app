f18e90b7b5ee7ff1259bca279fb38bcb884f58f6
Richard Potter <richardpotter.au@gmail.com>
2023-07-13 08:56:45 +1000

    start implementing inputs form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,21 @@
 import React from "react";
 
+import classes from "./Form.module.css";
+
 function Form() {
-  return <h1>Form</h1>;
+  return (
+    <form className={classes.form}>
+      <div className={classes.inputsContainer}>
+        <label htmlFor="day">Day</label>
+        <input type="text" id="day" placeholder="day" />
+        <label htmlFor="month">Month</label>
+        <input type="text" id="month" placeholder="month" />
+        <label htmlFor="year">Year</label>
+        <input type="text" id="year" placeholder="year" />
+      </div>
+      <button>Submit</button>
+    </form>
+  );
 }
 
 export default Form;
