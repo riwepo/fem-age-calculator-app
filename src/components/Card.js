50820272ab060395afa1f13ce22bcacfdcff503d
Richard Potter <richardpotter.au@gmail.com>
2023-07-13 08:32:15 +1000

    make car main element import classes

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,12 +3,14 @@ import React from "react";
 import Form from "./Form";
 import Display from "./Display";
 
+import classes from "./Card.module.css";
+
 function Card() {
   return (
-    <>
+    <main className={classes.card}>
       <Form />
       <Display />
-    </>
+    </main>
   );
 }
 
