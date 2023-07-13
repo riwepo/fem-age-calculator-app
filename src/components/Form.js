3c459a167d600503464ad250bf1a644981a562e2
Richard Potter <richardpotter.au@gmail.com>
2023-07-14 07:56:48 +1000

    begin day validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 import buttonImg from "../assets/images/icon-arrow.svg";
 
 import classes from "./Form.module.css";
 
 function Form() {
+  const [enteredDays, setEnteredDays] = useState("1");
+  const [enteredDaysIsTouched, setEnteredDaysIsTouched] = useState(false);
+  const [enteredDaysError, setEnteredDaysError] = useState("");
+
+  const dayInputChangeHandler = (event) => {
+    setEnteredDaysIsTouched(true);
+    const value = event.target.value;
+    if (enteredDays.trim() === "") {
+      setEnteredDaysError("This field is required");
+    }
+    const numericValue = +value;
+    if (numericValue < 1) {
+      setEnteredDaysError("Must be a valid day");
+    }
+  };
+
   return (
     <form className={classes.form}>
       <div className={classes.inputsContainer}>
         <label htmlFor="day">DAY</label>
-        <input type="text" id="day" placeholder="day" />
+        <input
+          type="number"
+          id="day"
+          placeholder="day"
+          onChange={dayInputChangeHandler}
+        />
         <label htmlFor="month">MONTH</label>
-        <input type="text" id="month" placeholder="month" />
+        <input type="number" id="month" placeholder="month" />
         <label htmlFor="year">YEAR</label>
-        <input type="text" id="year" placeholder="year" />
+        <input type="number" id="year" placeholder="year" />
       </div>
       <hr />
       <div className={classes.buttonContainer}>
