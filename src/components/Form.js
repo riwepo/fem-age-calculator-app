61c428e78354276e13218d8a5c3093c45e9e0123
Richard Potter <richardpotter.au@gmail.com>
2023-07-13 15:46:53 +1000

    move assets inside src style form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,26 @@
 import React from "react";
 
+import buttonImg from "../../assets/images/icon-arrow.svg";
+
 import classes from "./Form.module.css";
 
 function Form() {
   return (
     <form className={classes.form}>
       <div className={classes.inputsContainer}>
-        <label htmlFor="day">Day</label>
+        <label htmlFor="day">DAY</label>
         <input type="text" id="day" placeholder="day" />
-        <label htmlFor="month">Month</label>
+        <label htmlFor="month">MONTH</label>
         <input type="text" id="month" placeholder="month" />
-        <label htmlFor="year">Year</label>
+        <label htmlFor="year">YEAR</label>
         <input type="text" id="year" placeholder="year" />
       </div>
-      <button>Submit</button>
+      <div className={classes.buttonContainer}>
+        <hr />
+        <button className={classes.button}>
+          <img src={buttonImg} alt="arrow" />
+        </button>
+      </div>
     </form>
   );
 }
