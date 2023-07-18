4f1aa17acbef4137d32df7ba54e652a870a99bd7
Richard Potter <richardpotter.au@gmail.com>
2023-07-18 12:20:58 +1000

    manage user input to day and year

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,6 +4,28 @@ import buttonImg from "../assets/images/icon-arrow.svg";
 
 import classes from "./Form.module.css";
 
+function keyIsDeleteOrNumeric(key) {
+  // returns true if the key represents a delete, backspace or numeric char
+  const ascii = key.charCodeAt(0);
+  if (ascii === 66 || ascii === 68) {
+    return true;
+  }
+  return ascii >= 48 && ascii <= 66;
+}
+
+function addOrRemoveLeadingZero(trimmedChars) {
+  // adds or removes leading zero as required
+  if (trimmedChars.length === 1) {
+    return "0" + trimmedChars;
+  }
+
+  if (trimmedChars.length > 2 && trimmedChars[0] === "0") {
+    return trimmedChars.slice(1);
+  }
+
+  return trimmedChars;
+}
+
 function Form() {
   const [enteredDays, setEnteredDays] = useState("");
   const [enteredDaysIsTouched, setEnteredDaysIsTouched] = useState(false);
@@ -15,34 +37,51 @@ function Form() {
   const [enteredYearsIsTouched, setEnteredYearsIsTouched] = useState(false);
   const [enteredYearsError, setEnteredYearsError] = useState("");
 
+  const numericInputKeyHandler = (event) => {
+    // ignores non numeric keys
+    if (!keyIsDeleteOrNumeric(event.key)) {
+      event.preventDefault();
+    }
+  };
+
   const dayInputChangeHandler = (event) => {
+    // expects empty string or string of digits
     setEnteredDaysIsTouched(true);
-    const value = event.target.value;
-    if (value.trim() === "") {
+    const trimmedValue = event.target.value.trim();
+    setEnteredDays(trimmedValue);
+    if (trimmedValue === "") {
       setEnteredDaysError("This field is required");
       return;
     }
-    const numericValue = +value;
+    const numericValue = +trimmedValue;
+    console.log(numericValue);
     if (numericValue < 1) {
       setEnteredDaysError("Must be a valid day");
       return;
     }
 
+    setEnteredDays(addOrRemoveLeadingZero(trimmedValue));
+
     setEnteredDaysError("");
   };
 
   const monthInputChangeHandler = (event) => {
+    // expects empty string or string of digits
     setEnteredMonthsIsTouched(true);
-    const value = event.target.value;
-    if (value.trim() === "") {
+    const trimmedValue = event.target.value.trim();
+    setEnteredMonths(trimmedValue);
+    if (trimmedValue === "") {
       setEnteredMonthsError("This field is required");
       return;
     }
-    const numericValue = +value;
-    if (numericValue < 1) {
+    const numericValue = +trimmedValue;
+    console.log(numericValue);
+    if (numericValue < 1 || numericValue > 12) {
       setEnteredMonthsError("Must be a valid month");
       return;
     }
+
+    setEnteredMonths(addOrRemoveLeadingZero(trimmedValue));
 
     setEnteredMonthsError("");
   };
@@ -76,12 +115,16 @@ function Form() {
         </label>
         <input
           type="number"
+          min={1}
+          max={31}
+          value={enteredDays}
           id="day"
           placeholder="DD"
           className={`${classes.dayInput} ${
             enteredDaysError !== "" ? classes.dayInputError : ""
           }`}
           onChange={dayInputChangeHandler}
+          onKeyDown={numericInputKeyHandler}
         />
         {enteredDaysError !== "" && (
           <p className={classes.dayErrorMessage}>{enteredDaysError}</p>
@@ -97,12 +140,16 @@ function Form() {
         </label>
         <input
           type="number"
+          min={1}
+          max={12}
+          value={enteredMonths}
           id="month"
           placeholder="MM"
           className={`${classes.monthInput} ${
             enteredMonthsError !== "" ? classes.monthInputError : ""
           }`}
           onChange={monthInputChangeHandler}
+          onKeyDown={numericInputKeyHandler}
         />
         {enteredMonthsError !== "" && (
           <p className={classes.monthErrorMessage}>{enteredMonthsError}</p>
