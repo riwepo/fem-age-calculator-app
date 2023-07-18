f623a7ca3ddb2f544b561e31e14265d1d39e5c71
Richard Potter <richardpotter.au@gmail.com>
2023-07-18 14:51:15 +1000

    store date from form in local state

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Form from "./Form";
 import Display from "./Display";
@@ -6,13 +6,15 @@ import Display from "./Display";
 import classes from "./Card.module.css";
 
 function Card() {
+  const [enteredDate, setEnteredDate] = useState(null);
   function onDateEntered(date) {
     console.log(date);
+    setEnteredDate(date);
   }
   return (
     <main className={classes.card}>
       <Form onSubmit={onDateEntered} />
-      {/* <Display /> */}
+      <Display date={enteredDate} />
     </main>
   );
 }
