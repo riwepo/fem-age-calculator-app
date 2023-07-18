ec2ab60d67c680374d3e54bde06441ef0eec8368
Richard Potter <richardpotter.au@gmail.com>
2023-07-18 16:17:26 +1000

    if date not valid display dashes

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,8 +1,51 @@
 import React from "react";
 
+function dateDiff(earlierDate, laterDate) {
+  const startYear = earlierDate.getFullYear();
+  const february =
+    (startYear % 4 === 0 && startYear % 100 !== 0) || startYear % 400 === 0
+      ? 29
+      : 28;
+  const daysInMonth = [31, february, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  let yearDiff = laterDate.getFullYear() - startYear;
+  let monthDiff = laterDate.getMonth() - earlierDate.getMonth();
+  if (monthDiff < 0) {
+    yearDiff--;
+    monthDiff += 12;
+  }
+  let dayDiff = laterDate.getDate() - earlierDate.getDate();
+  if (dayDiff < 0) {
+    if (monthDiff > 0) {
+      monthDiff--;
+    } else {
+      yearDiff--;
+      monthDiff = 11;
+    }
+    dayDiff += daysInMonth[earlierDate.getMonth()];
+  }
+
+  return { years: yearDiff, months: monthDiff, days: dayDiff };
+}
+
 function Display({ date }) {
-  const dateString = date ? date.toString() : "";
-  return <h1>{dateString}</h1>;
+  const dateDiffData = date ? dateDiff(date, new Date()) : null;
+  const years = dateDiffData ? dateDiffData.years : "--";
+  const months = dateDiffData ? dateDiffData.months : "--";
+  const days = dateDiffData ? dateDiffData.days : "--";
+  return (
+    <>
+      <p>
+        <span>{years}</span> years
+      </p>
+      <p>
+        <span>{months}</span> months
+      </p>
+      <p>
+        <span>{days}</span> days
+      </p>
+    </>
+  );
 }
 
 export default Display;
